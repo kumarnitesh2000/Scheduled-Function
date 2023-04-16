693bef50019687f85244c23ef00b4324ed947c3e
unknown <kumarnitesh2000.nk@gmail.com>
2023-04-16 16:00:16 +0530

    init

--- netlify/functions/recommend.js
+++ netlify/functions/recommend.js
@@ -1,16 +1,10 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/test-scheduled-function.js
 const { schedule } = require("@netlify/functions");
-const { addDataToSheet } = require("./googlesheet_updater");
 
 let count = 1;
 let cron = "* * * * *";
 const handler = async function(event, context) {
-    // console.log("Received event:", event);
     console.log("cron counter:", count)
     count++;
-
-    await addDataToSheet()
-
     return {
         statusCode: 200,
     };
